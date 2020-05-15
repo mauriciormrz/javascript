775e1f2e7182859db6f178ff685bab327e311de2
Mauricio Ramirez <ramirezmauricio@hotmail.com>
2020-05-15 06:48:33 -0500

    Empezar aqui

---
+++ app.js
@@ -0,0 +1,16 @@
+
+function imprimir(){
+
+	for (var i=0; i<3000; i++){
+		console.log("Imprimir");
+	}
+}
+
+function presionoClick(){
+
+	console.log("Click en botón");
+}
+
+var nombre = "Mauricio";
+
+imprimir();
