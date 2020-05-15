774ae5001737dca7f30c20dadf2accb4d6a646e5
Mauricio Ramirez <ramirezmauricio@hotmail.com>
2020-05-15 18:52:55 -0500

    El señor de los Anillos

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
 
-function imprimir(){
+function Jugador(nombre){
 
-	for (var i=0; i<3000; i++){
-		console.log("Imprimir");
+	this.nombre = nombre;
+	this.pv = 100;
+	this.sp = 100;
+
+	this.curar = function(jugadorObjetivo){
+
+		if (this.sp >= 40){
+			this.sp -= 40;
+			jugadorObjetivo.pv += 20;
+		}else {
+			console.info(this.nombre + " no tiene sp");
+		}
+
+		this.estado(jugadorObjetivo);
+	}
+
+	this.estado = function(jugadorObjetivo){
+
+		console.info(this);
+		console.info(jugadorObjetivo);
+	}
+
+	this.tirarFlecha = function(jugadorObjetivo){
+
+		if (jugadorObjetivo.pv >= 40){
+			jugadorObjetivo.pv -= 40;
+		} else {
+			jugadorObjetivo.pv = 0;
+			console.error(jugadorObjetivo.nombre + " murió!!");
+		}
+		this.estado(jugadorObjetivo);
 	}
 }
 
-function presionoClick(){
 
-	console.log("Click en botón");
-}
 
-var nombre = "Mauricio";
+var gandalf = new Jugador("Gandalf");
+var legolas = new Jugador("Legolas");
 
-imprimir();
+console.log(gandalf);
+console.log(legolas);
+
+gandalf.curar(legolas);
